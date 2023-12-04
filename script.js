94f602683f123b1f1d62c516285ae39214a61b0c
CaramonH <Caramonh@yahoo.com>
2023-12-04 15:37:07 -0600

    cleaned up Task 1

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
 $(document).ready(function(){
-    PopulateQuotes();
+    populateQuotes();
 });
 
-function PopulateQuotes() {
-    $.ajax({
+function fetchQuotes() {
+    return $.ajax({
         url: "https://smileschool-api.hbtn.info/quotes",
         method: "GET",
-        success: function(response){
-            const quoteCarousel = $('#quote-carousel');
+    });
+}
 
-            response.forEach(function makeCarouselItem(quote, index) {
-                const carouselItem = $('<div>').addClass('carousel-item');
-                const rowItem = $('<div>').addClass('row mx-auto align-items-center');
-                const picDiv = $('<div>').addClass('col-12 col-sm-2 col-lg-2 offset-lg-1 text-center');
-                const quoteImg = $('<img>').addClass('d-block align-self-center').attr('src', quote['pic_url']);
-                const quoteDiv = $('<div>').addClass('col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0');
-                const quoteTextDiv = $('<div>').addClass('quote-text');
-                const quotePrg = $('<p>').addClass('text-white').text(quote['text']);
-                const quoteeName = $('<h4>').addClass('text-white font-weight-bold').text(quote['name']);
-                const quoteeTitle = $('<span>').addClass('text-white').text(quote['title']);
-                
-                quoteTextDiv.append(quotePrg, quoteeName, quoteeTitle);
-                quoteDiv.append(quoteTextDiv);
-                picDiv.append(quoteImg);
-                rowItem.append(picDiv, quoteDiv);
-                carouselItem.append(rowItem);
-                quoteCarousel.append(carouselItem);
+function createCarouselItem(quote, isActive) {
+    const { pic_url, text, name, title } = quote;
 
-                if (index === 0) {
-                    carouselItem.addClass('active');
-                }
-            });
-            $('#loading-quotes').addClass('d-none');
-            $('#quotes-container').removeClass('d-none');
-        },
-        error: function() {
-            alert("Error loading quotes");
-        }
-    })
+    const carouselItem = $('<div>', { class: `carousel-item ${isActive ? 'active' : ''}` });
+    const rowItem = $('<div>', { class: 'row mx-auto align-items-center' });
+    const picDiv = $('<div>', { class: 'col-12 col-sm-2 col-lg-2 offset-lg-1 text-center' });
+    const quoteImg = $('<img>', { class: 'd-block align-self-center', src: pic_url });
+    const quoteDiv = $('<div>', { class: 'col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0' });
+    const quoteTextDiv = $('<div>', { class: 'quote-text' });
+    const quotePrg = $('<p>', { class: 'text-white' }).text(text);
+    const quoteeName = $('<h4>', { class: 'text-white font-weight-bold' }).text(name);
+    const quoteeTitle = $('<span>', { class: 'text-white' }).text(title);
+
+    quoteTextDiv.append(quotePrg, quoteeName, quoteeTitle);
+    quoteDiv.append(quoteTextDiv);
+    picDiv.append(quoteImg);
+    rowItem.append(picDiv, quoteDiv);
+    carouselItem.append(rowItem);
+
+    return carouselItem;
+}
+
+function displayQuotes(quotes) {
+    const quoteCarousel = $('#quote-carousel');
+    quotes.forEach((quote, index) => {
+        const isActive = index === 0;
+        const carouselItem = createCarouselItem(quote, isActive);
+        quoteCarousel.append(carouselItem);
+    });
+
+    $('#loading-quotes').addClass('d-none');
+    $('#quotes-container').removeClass('d-none');
+}
+
+function handleFetchError() {
+    alert("Error loading quotes");
+}
+
+function populateQuotes() {
+    fetchQuotes()
+        .then(displayQuotes)
+        .catch(handleFetchError);
 }
